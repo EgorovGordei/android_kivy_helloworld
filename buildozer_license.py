006f7551c01e76161d762bdfcbb00978c1584004
EgorovGordei <78762468+EgorovGordei@users.noreply.github.com>
2021-08-12 22:18:29 +0300

    Update buildozer_license.py

--- buildozer_license.py
+++ buildozer_license.py
@@ -1,15 +1,6 @@
 from subprocess import Popen, PIPE, STDOUT
 
 
-# change log_level = 2
-f = open("buildozer.spec", 'r')
-s = f.read()
-f.close()
-s = s.replace("log_level = 1", "log_level = 2")
-f = open("buildozer.spec", 'w')
-f.write(s)
-f.close()
-
 # run buildozer via Popen to provide agreement to license
 p = Popen(['buildozer', 'android', 'debug', 'deploy', 'run'], stdout=PIPE, stdin=PIPE, stderr=STDOUT)    
 grep_stdout = p.communicate(input=b'y\n')[0]
