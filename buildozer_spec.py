8c120c78a8951d4353d08e9d37025dedd5f25a7f
EgorovGordei <78762468+EgorovGordei@users.noreply.github.com>
2021-08-13 01:11:03 +0300

    Update and rename buildozer_log_level.py to buildozer_spec.py

--- buildozer_spec.py
+++ buildozer_spec.py
@@ -1,9 +1,10 @@
-# change log_level = 2
 f = open("buildozer.spec", 'r')
 s = f.read()
 f.close()
+
 s = s.replace("log_level = 1", "log_level = 2")
 s = s.replace("# android.accept_sdk_license = False", "android.accept_sdk_license = True")
+
 f = open("buildozer.spec", 'w')
 f.write(s)
 f.close()
