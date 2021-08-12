0c69d09f5dc7858ae5456b9c767929fa64116958
EgorovGordei <78762468+EgorovGordei@users.noreply.github.com>
2021-08-13 01:33:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
+import kivy
+kivy.require("1.9.1")
+
 from kivy.app import App
-from kivy.uix.label import Label
+from kivy.uix.button import Button
 
-class MainApp(App):
+
+class ButtonApp(App):
     def build(self):
-        label = Label(text='Hello from Kivy',
-                      size_hint=(.5, .5),
-                      pos_hint={'center_x': .5, 'center_y': .5})
+        self.btn = Button(text ="Tap",
+                   font_size ="20sp",
+                   background_color =(1, 0.1, 0.1, 1),
+                   color =(1, 1, 1, 1),
+                   size =(32, 32),
+                   size_hint =(.2, .2),
+                   pos =(300, 250))
+        self.btn.bind(on_press = self.callback)
+        return self.btn
 
-        return label
+    def callback(self, event):
+        if self.btn.text == "Tap":
+            self.btn.text = "1"
+        else:
+            self.btn.text = str(int(self.btn.text) + 1)
 
-if __name__ == '__main__':
-    app = MainApp()
-    app.run()
+
+root = ButtonApp()
+root.run()
