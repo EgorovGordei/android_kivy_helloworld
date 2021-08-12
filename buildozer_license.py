66877caf1ba231cf5a3d1df45a24490d11060354
EgorovGordei <78762468+EgorovGordei@users.noreply.github.com>
2021-08-12 21:18:17 +0300

    Add files via upload

---
+++ buildozer_license.py
@@ -0,0 +1,5 @@
+from subprocess import Popen, PIPE, STDOUT
+
+p = Popen(['run.bat'], stdout=PIPE, stdin=PIPE, stderr=STDOUT)    
+grep_stdout = p.communicate(input=b'one\ntwo\nthree\nfour\nfive\nsix\n')[0]
+print(grep_stdout)
