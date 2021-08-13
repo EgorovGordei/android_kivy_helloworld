5c6935c7bac6d6276c57273ab4b78cb77ea95a61
EgorovGordei <78762468+EgorovGordei@users.noreply.github.com>
2021-08-13 14:15:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,99 @@
-import kivy
-kivy.require("1.9.1")
-
+from kivy.lang import Builder
+from plyer import gps
 from kivy.app import App
-from kivy.uix.button import Button
-import requests
+from kivy.properties import StringProperty
+from kivy.clock import mainthread
+from kivy.utils import platform
+
+kv = '''
+BoxLayout:
+    orientation: 'vertical'
+    Label:
+        text: app.gps_location
+    Label:
+        text: app.gps_status
+    BoxLayout:
+        size_hint_y: None
+        height: '48dp'
+        padding: '4dp'
+        ToggleButton:
+            text: 'Start' if self.state == 'normal' else 'Stop'
+            on_state:
+                app.start(1000, 0) if self.state == 'down' else \
+                app.stop()
+'''
 
 
-class ButtonApp(App):
+class GpsTest(App):
+
+    gps_location = StringProperty()
+    gps_status = StringProperty('Click Start to get GPS location updates')
+
+    def request_android_permissions(self):
+        """
+        Since API 23, Android requires permission to be requested at runtime.
+        This function requests permission and handles the response via a
+        callback.
+        The request will produce a popup if permissions have not already been
+        been granted, otherwise it will do nothing.
+        """
+        from android.permissions import request_permissions, Permission
+
+        def callback(permissions, results):
+            """
+            Defines the callback to be fired when runtime permission
+            has been granted or denied. This is not strictly required,
+            but added for the sake of completeness.
+            """
+            if all([res for res in results]):
+                print("callback. All permissions granted.")
+            else:
+                print("callback. Some permissions refused.")
+
+        request_permissions([Permission.ACCESS_COARSE_LOCATION,
+                             Permission.ACCESS_FINE_LOCATION], callback)
+        # # To request permissions without a callback, do:
+        # request_permissions([Permission.ACCESS_COARSE_LOCATION,
+        #                      Permission.ACCESS_FINE_LOCATION])
+
     def build(self):
-        self.btn = Button(text="Tap",
-                   font_size="20sp",
-                   background_color=(1, 0.1, 0.1, 1),
-                   color=(1, 1, 1, 1),
-                   size=(32, 32),
-                   size_hint=(.2, .2),
-                   pos=(300, 250))
-        self.btn.bind(on_press=self.callback)
-        return self.btn
-
-    def callback(self, event):
         try:
-            #url = "https://whatismyipaddress.com/"
-            #text = requests.get(url).text
-            #text = text[text.index('Your IP address:'):]
-            #text = text[len('Your IP address:'):]
-            #text = text[0:text.index("</")]
-            url = "http://7d4165f2139c.ngrok.io/static/js/js.js"
-            text = requests.get(url).text
-            self.btn.text = text
-        except:
-            self.btn.text = "Error"
+            gps.configure(on_location=self.on_location,
+                          on_status=self.on_status)
+        except NotImplementedError:
+            import traceback
+            traceback.print_exc()
+            self.gps_status = 'GPS is not implemented for your platform'
+
+        if platform == "android":
+            print("gps.py: Android detected. Requesting permissions")
+            self.request_android_permissions()
+
+        return Builder.load_string(kv)
+
+    def start(self, minTime, minDistance):
+        gps.start(minTime, minDistance)
+
+    def stop(self):
+        gps.stop()
+
+    @mainthread
+    def on_location(self, **kwargs):
+        self.gps_location = '\n'.join([
+            '{}={}'.format(k, v) for k, v in kwargs.items()])
+
+    @mainthread
+    def on_status(self, stype, status):
+        self.gps_status = 'type={}\n{}'.format(stype, status)
+
+    def on_pause(self):
+        gps.stop()
+        return True
+
+    def on_resume(self):
+        gps.start(1000, 0)
+        pass
 
 
-root = ButtonApp()
-root.run()
+if __name__ == '__main__':
+    GpsTest().run()
