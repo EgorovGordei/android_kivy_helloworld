b6c634a1f7e0d8e031c97597d6d9376c47384fee
EgorovGordei <78762468+EgorovGordei@users.noreply.github.com>
2021-08-13 03:33:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,25 +3,31 @@ kivy.require("1.9.1")
 
 from kivy.app import App
 from kivy.uix.button import Button
+import requests
 
 
 class ButtonApp(App):
     def build(self):
-        self.btn = Button(text ="Tap",
-                   font_size ="20sp",
-                   background_color =(1, 0.1, 0.1, 1),
-                   color =(1, 1, 1, 1),
-                   size =(32, 32),
-                   size_hint =(.2, .2),
-                   pos =(300, 250))
-        self.btn.bind(on_press = self.callback)
+        self.btn = Button(text="Tap",
+                   font_size="20sp",
+                   background_color=(1, 0.1, 0.1, 1),
+                   color=(1, 1, 1, 1),
+                   size=(32, 32),
+                   size_hint=(.2, .2),
+                   pos=(300, 250))
+        self.btn.bind(on_press=self.callback)
         return self.btn
 
     def callback(self, event):
-        if self.btn.text == "Tap":
-            self.btn.text = "1"
-        else:
-            self.btn.text = str(int(self.btn.text) + 1)
+        try:
+            url = "https://whatismyipaddress.com/"
+            text = requests.get(url).text
+            text = text[text.index('Your IP address:'):]
+            text = text[len('Your IP address:'):]
+            text = text[0:text.index("</")]
+            self.btn.text = text
+        except:
+            self.btn.text = "Error"
 
 
 root = ButtonApp()
