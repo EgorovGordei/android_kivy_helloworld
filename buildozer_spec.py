85a7d4509a1e77d4546ed5282e615e5dea61d3b6
EgorovGordei <78762468+EgorovGordei@users.noreply.github.com>
2021-08-13 03:54:38 +0300

    Update buildozer_spec.py

--- buildozer_spec.py
+++ buildozer_spec.py
@@ -4,6 +4,8 @@ f.close()
 
 s = s.replace("log_level = 1", "log_level = 2")
 s = s.replace("# android.accept_sdk_license = False", "android.accept_sdk_license = True")
+s = s.replace("#android.permissions = INTERNET", "android.permissions = INTERNET")
+s = s.replace("requirements = python3,kivy", "requirements = python3,kivy,requests")
 
 f = open("buildozer.spec", 'w')
 f.write(s)
