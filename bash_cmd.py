8ae4cbb41ebd4594929ac98f2d452e1a8be83b33
EgorovGordei <78762468+EgorovGordei@users.noreply.github.com>
2021-08-14 14:30:54 +0300

    Update bash_cmd.py

--- bash_cmd.py
+++ bash_cmd.py
@@ -5,5 +5,22 @@ bashCmd = ["toolchain", "recipes"]
 process = subprocess.Popen(bashCmd, stdout=subprocess.PIPE)
 output, error = process.communicate()
 
+print("DBG:OUTPUT:", output)
 
-print("DBG:OUTPUT_ERROR:", output, error)
+output = output.decode(errors="ignore")
+
+print("DBG:OUTPUT:", output)
+
+counter = 0
+new_output = ""
+for letter in output:
+    if counter == 0 and letter == " ":
+        counter = 1
+        new_output += " "
+    else if counter == 1 and letter == "\n":
+        counter = 0
+    else if counter == 0:
+        new_output += letter
+output = new_output
+
+print("DBG:OUTPUT:", output)
