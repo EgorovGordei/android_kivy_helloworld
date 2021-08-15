fd60969d3c2418e0206f59c83885bdcb78c93af1
EgorovGordei <78762468+EgorovGordei@users.noreply.github.com>
2021-08-15 15:43:05 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,45 +1,49 @@
 import kivy
 from kivy.lang import Builder
 from plyer import gps
+from plyer import camera
 from kivy.app import App
 from kivy.properties import StringProperty
 from kivy.clock import mainthread
 from kivy.utils import platform
 import requests
 from kivymd.app import MDApp
+import os
 
 
 mainkv = """
 BoxLayout:
     orientation: 'vertical'
     MDLabel:
-        text: app.gps_location
+        text: app.current_dir
     Button:
-        text: "Just kivy settings"
-        on_release: app.open_settings()
+        text: app.button_text
+        on_release: app.take_image()
  """
 
-class GpsTest(MDApp):
-    gps_location = StringProperty('None')
+class CameraTest(MDApp):
+    current_dir = StringProperty("None")
+    button_text = StringProperty("None")
     def build(self):
-        try:
-            gps.configure(on_location=self.on_location)
+        if platform == "android":
             from android.permissions import request_permissions, Permission
-            request_permissions([Permission.ACCESS_COARSE_LOCATION,
-                             Permission.ACCESS_FINE_LOCATION])
-        except NotImplementedError:
-            self.gps_location = "Not implemented"
+            request_permissions([Permission.CAMERA,
+                                 Permission.WRITE_EXTERNAL_STORAGE,
+                                 Permission.READ_EXTERNAL_STORAGE])
+        try:
+            self.current_dir = os.path.dirname(__file__)
+        except Exception as e:
+            self.current_dir = "Current dir not implemented (lol?)"
+            print(e)
         return Builder.load_string(mainkv)
-    @mainthread
-    def on_location(self, **kwargs):
-        self.gps_location = '\n'.join([
-            '{}={}'.format(k, v) for k, v in kwargs.items()])
-        print(self.gps_location)
-    def on_resume(self):
-        gps.start()
-        pass
-    def on_pause(self):
-        gps.stop()
-        return True
+    def take_image(self):
+        self.button_text = "Taking picture"
+        try:
+            camera.take_picture(filename="tmp_31415.jpg",
+                                on_complete=lambda *a: a)
+            self.button_text = "Picture was taken"
+        except NotImplementedError:
+            self.button_text = "Camera not implemented"
         
-GpsTest().run()
+        
+CameraTest().run()
