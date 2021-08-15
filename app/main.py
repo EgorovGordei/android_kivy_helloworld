f36ec91f99f8011f581e74e9a2b00a058a7ac80c
EgorovGordei <78762468+EgorovGordei@users.noreply.github.com>
2021-08-15 17:07:55 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,49 +1,55 @@
 import kivy
 from kivy.lang import Builder
-from plyer import gps
-from plyer import camera
 from kivy.app import App
 from kivy.properties import StringProperty
 from kivy.clock import mainthread
 from kivy.utils import platform
-import requests
 from kivymd.app import MDApp
+from kivy.uix.boxlayout import BoxLayout
+import time
 import os
+
+from plyer import gps
+from plyer import camera
+import requests
 
 
 mainkv = """
-BoxLayout:
+<CameraClick>:
     orientation: 'vertical'
-    MDLabel:
-        text: app.current_dir
+    Camera:
+        id: camera
+        resolution: (640, 480)
+        play: False
+    ToggleButton:
+        text: 'Play'
+        on_press: camera.play = not camera.play
+        size_hint_y: None
+        height: '48dp'
     Button:
-        text: app.button_text
-        on_release: app.take_image()
- """
+        text: 'Capture'
+        size_hint_y: None
+        height: '48dp'
+        on_press: root.capture()
+"""
 
-class CameraTest(MDApp):
-    current_dir = StringProperty("None")
-    button_text = StringProperty("None")
+
+class CameraClick(BoxLayout):
+    def capture(self):
+        camera = self.ids['camera']
+        timestr = time.strftime("%Y%m%d_%H%M%S")
+        camera.export_to_png("IMG_{}.png".format(timestr))
+
+
+class TestCamera(MDApp):
     def build(self):
         if platform == "android":
             from android.permissions import request_permissions, Permission
             request_permissions([Permission.CAMERA,
                                  Permission.WRITE_EXTERNAL_STORAGE,
                                  Permission.READ_EXTERNAL_STORAGE])
-        try:
-            self.current_dir = os.path.dirname(__file__)
-        except Exception as e:
-            self.current_dir = "Current dir not implemented (lol?)"
-            print(e)
-        return Builder.load_string(mainkv)
-    def take_image(self):
-        self.button_text = "Taking picture"
-        try:
-            camera.take_picture(filename="tmp_31415.jpg",
-                                on_complete=lambda *a: a)
-            self.button_text = "Picture was taken"
-        except NotImplementedError:
-            self.button_text = "Camera not implemented"
-        
-        
-CameraTest().run()
+        Builder.load_string(mainkv)
+        return CameraClick()
+
+
+TestCamera().run()
