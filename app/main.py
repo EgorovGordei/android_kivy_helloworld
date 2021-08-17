bbd263564dbde5ff1e43824a08d771daec07b985
EgorovGordei <78762468+EgorovGordei@users.noreply.github.com>
2021-08-18 00:28:40 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,45 +1,34 @@
 import kivy
-from kivy.lang import Builder
-from plyer import gps
+kivy.require("1.9.1")
+
 from kivy.app import App
-from kivy.properties import StringProperty
-from kivy.clock import mainthread
-from kivy.utils import platform
+from kivy.uix.button import Button
 import requests
-from kivymd.app import MDApp
 
 
-mainkv = """
-BoxLayout:
-    orientation: 'vertical'
-    MDLabel:
-        text: app.gps_location
-    Button:
-        text: "Just kivy settings"
-        on_release: app.open_settings()
- """
-
-class GpsTest(MDApp):
-    gps_location = StringProperty('None')
+class ButtonApp(App):
     def build(self):
+        self.btn = Button(text="Tap",
+                   font_size="20sp",
+                   background_color=(1, 0.1, 0.1, 1),
+                   color=(1, 1, 1, 1),
+                   size=(32, 32),
+                   size_hint=(.2, .2),
+                   pos=(300, 250))
+        self.btn.bind(on_press=self.callback)
+        return self.btn
+
+    def callback(self, event):
         try:
-            gps.configure(on_location=self.on_location)
-            from android.permissions import request_permissions, Permission
-            request_permissions([Permission.ACCESS_COARSE_LOCATION,
-                             Permission.ACCESS_FINE_LOCATION])
-        except NotImplementedError:
-            self.gps_location = "Not implemented"
-        return Builder.load_string(mainkv)
-    @mainthread
-    def on_location(self, **kwargs):
-        self.gps_location = '\n'.join([
-            '{}={}'.format(k, v) for k, v in kwargs.items()])
-        print(self.gps_location)
-    def on_resume(self):
-        gps.start()
-        pass
-    def on_pause(self):
-        gps.stop()
-        return True
-        
-GpsTest().run()
+            url = "https://whatismyipaddress.com/"
+            text = requests.get(url).text
+            text = text[text.index('Your IP address:'):]
+            text = text[len('Your IP address:'):]
+            text = text[0:text.index("</")]
+            self.btn.text = text
+        except:
+            self.btn.text = "Error"
+
+
+root = ButtonApp()
+root.run()
