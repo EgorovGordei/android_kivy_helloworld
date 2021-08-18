35a87f69ccbb94ed21bc53a37fe921e9d2e3f72c
EgorovGordei <78762468+EgorovGordei@users.noreply.github.com>
2021-08-19 02:20:43 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,95 +1,146 @@
-import kivy
-from kivy.lang import Builder
 from kivy.app import App
-from kivy.properties import StringProperty
-from kivy.clock import mainthread
-from kivy.utils import platform
-from kivymd.app import MDApp
+from kivy.lang import Builder
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.uix.popup import Popup
 from kivy.uix.boxlayout import BoxLayout
-import time
-import os
-
-from plyer import gps
-from plyer import camera
-import requests
-
-from kivy.graphics.texture import Texture
-from kivy.clock import Clock
-import numpy as np
-import cv2
-
-import imutils
-
-from plyer import accelerometer
-from plyer import gravity
+from plyer import wifi
+from functools import partial
 
 
-main_kv = '''
-<GravityInterface>:
+Builder.load_string('''
+<WifiInterface>:
     orientation: 'vertical'
-    MDLabel:
-        id: x_label
-        text: 'X: '
-    MDLabel:
-        id: y_label
-        text: 'Y: '
-    MDLabel:
-        id: z_label
-        text: 'Z: '
-    MDLabel:
-        id: status
-        text: ''
-    BoxLayout:
+    padding: '30dp'
+    spacing: '20dp'
+    GridLayout:
+        cols: 2
+        padding: 20
+        spacing: 20
+        size_hint: 1,.4
+        Button:
+            text: "Disconnect"
+            on_release: root.disconnect()
+        TextInput:
+            id: password
+            hint_text: "Password"
+            disabled: True
+    Label:
         size_hint_y: None
-        height: '48dp'
-        padding: '4dp'
-        ToggleButton:
-            id: toggle_button
-            text: 'Start Gravity Sensor'
-            on_press: root.do_toggle()
-'''
+        height: sp(20)
+        text: 'Wifi enabled: ' + str(root.is_enabled())
+    BoxLayout:
+        orientation: 'horizontal'
+        size_hint_y: 0.3
+        Button:
+            id: wifi_button
+            size_hint_y: None
+            height: sp(35)
+            text: 'Enable Wifi / Start Scanning'
+            on_release: root.start_wifi()
+        Button:
+            id: stop_wifi_button
+            size_hint_y: None
+            height: sp(35)
+            disabled: True
+            text: 'Disable Wifi'
+            on_release: root.stop_wifi()
+    BoxLayout:
+        id: scan_layout
+        orientation: 'vertical'
+        Label:
+            size_hint_x: 1
+            size_hint_y: None
+            valign: 'middle'
+            height: '35dp'
+            text: 'Scan Results'
+''')
 
 
-class GravityInterface(BoxLayout):
-    def __init__(self):
-        super().__init__()
-        self.sensorEnabled = False
+class WifiInterface(BoxLayout):
 
-    def do_toggle(self):
-        try:
-            if not self.sensorEnabled:
-                gravity.enable()
-                Clock.schedule_interval(self.get_gravity, 1 / 20.)
+    param = {}
 
-                self.sensorEnabled = True
-                self.ids.toggle_button.text = "Stop Gravity Sensor"
-            else:
-                gravity.disable()
-                Clock.unschedule(self.get_gravity)
+    def _create_popup(self, title, content):
+        return Popup(
+            title=title,
+            content=Label(text=content),
+            size_hint=(.8, 1),
+            auto_dismiss=True
+        )
 
-                self.sensorEnabled = False
-                self.ids.toggle_button.text = "Start Gravity Sensor"
-        except NotImplementedError:
-            import traceback
-            traceback.print_exc()
-            status = "Gravity sensor is not implemented " \
-                     "for your platform"
-            self.ids.status.text = status
+    def start_wifi(self):
+        wifi_button = self.ids['wifi_button']
+        wifi_button.text = 'Showing Scan Results'
+        wifi_button.on_release = self.show_wifi_scans
+        wifi.start_scanning()
+        stop_wifi_button = self.ids['stop_wifi_button']
+        stop_wifi_button.disabled = False
+        text_inpt = self.ids['password']
+        text_inpt.disabled = False
 
-    def get_gravity(self, dt):
-        val = gravity.gravity
+    def stop_wifi(self):
+        stop_wifi_button = self.ids['stop_wifi_button']
+        stop_wifi_button.disabled = True
 
-        if not val == (None, None, None):
-            self.ids.x_label.text = "X: " + str(val[0])
-            self.ids.y_label.text = "Y: " + str(val[1])
-            self.ids.z_label.text = "Z: " + str(val[2])
+        wifi_button = self.ids['wifi_button']
+        wifi_button.text = 'Enable Wifi'
+        wifi_button.on_release = self.start_wifi
+
+        wifi.disable()
+        self.ids['scan_layout'].clear_widgets()
+        text_inpt = self.ids['password']
+        text_inpt.disabled = False
+
+    def start_scanning(self):
+        wifi.start_scanning()
+
+    def show_wifi_scans(self):
+        stack = self.ids['scan_layout']
+        stack.clear_widgets()
+        wifi_scans = wifi.names.keys()
+        for name in wifi_scans:
+            content = ""
+            items = wifi._get_network_info(name)
+            for key, value in items.items():
+                content += "{}:    {} \n".format(key, value)
+
+            popup = self._create_popup(name, content)
+            boxl = BoxLayout(orientation='horizontal')
+            button = Button(
+                text=name,
+                size_hint=(1, 1),
+                height='40dp',
+                on_release=popup.open,
+            )
+            button_connect = Button(
+                text="Connect",
+                size_hint_x=.2,
+                on_release=partial(self.connect, name))
+
+            boxl.add_widget(button)
+            boxl.add_widget(button_connect)
+            stack.add_widget(boxl)
+
+    def is_enabled(self):
+        return wifi.is_enabled()
+
+    def disconnect(self):
+        wifi.disconnect()
+
+    def connect(self, network_name, instance):
+        self.param['password'] = self.ids['password'].text
+        wifi.connect(network_name, self.param)
 
 
-class GravityApp(MDApp):
+class WifiApp(App):
+
     def build(self):
-        Builder.load_string(main_kv)
-        return GravityInterface()
+        return WifiInterface()
+
+    def on_pause(self):
+        return True
 
 
-if __name__ == '__main__':
-    GravityApp().run()
+if __name__ == "__main__":
+    WifiApp().run()
