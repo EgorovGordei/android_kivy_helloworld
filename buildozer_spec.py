ae657e67c7a34dc9960a0a801846ae2812745774
EgorovGordei <78762468+EgorovGordei@users.noreply.github.com>
2021-08-22 23:25:55 +0300

    Update buildozer_spec.py

--- buildozer_spec.py
+++ buildozer_spec.py
@@ -5,7 +5,7 @@ f.close()
 s = s.replace("log_level = 1", "log_level = 2")
 s = s.replace("# android.accept_sdk_license = False", "android.accept_sdk_license = True")
 s = s.replace("#android.permissions = INTERNET", "android.permissions = INTERNET,CAMERA,WRITE_EXTERNAL_STORAGE,READ_EXTERNAL_STORAGE,ACCESS_FINE_LOCATION,ACCESS_COARSE_LOCATION,RECORD_AUDIO")
-s = s.replace("requirements = python3,kivy", "requirements = mdeiapipe,libffi,imutils,opencv,numpy,cffi,python3==3.7.11,kivy==master,kivymd==0.104.2,sdl2_ttf==2.0.15,pillow,requests,urllib3,charset-normalizer,idna,plyer")
+s = s.replace("requirements = python3,kivy", "requirements = mediapipe,libffi,imutils,opencv,numpy,cffi,python3==3.7.11,kivy==master,kivymd==0.104.2,sdl2_ttf==2.0.15,pillow,requests,urllib3,charset-normalizer,idna,plyer")
 s = s.replace("#p4a.branch = master", "p4a.branch = develop")
 
 s = s.replace("#android.api = 27", "android.api = 30")
