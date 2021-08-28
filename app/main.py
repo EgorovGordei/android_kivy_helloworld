528d849cbb5c6b839ea3fb9ebc4425c7148da9f7
EgorovGordei <78762468+EgorovGordei@users.noreply.github.com>
2021-08-28 13:25:28 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -9,137 +9,158 @@ from kivy.uix.boxlayout import BoxLayout
 import time
 import os
 
-from kivy.graphics import Mesh, Color
-from kivy.graphics.tesselator import Tesselator, WINDING_ODD, TYPE_POLYGONS
-from kivy.uix.floatlayout import FloatLayout
+from plyer import gps
+from plyer import camera
+import requests
+
+from kivy.graphics.texture import Texture
+from kivy.uix.camera import Camera
+from kivy.clock import Clock
+import numpy as np
+import cv2
 
 
-Builder.load_string("""
-<ShapeBuilder>:
-    canvas.before:
-        Color:
-            rgba: 0, 1, 0, 1
-        Rectangle:
-            pos: self.pos
-            size: self.size
-    BoxLayout:
+import imutils
+
+
+mainkv = """
+<CameraClick>:
+    orientation: 'vertical'
+    Camera:
+        id: camera
+        resolution: (640, 480)
+        play: False
+    ToggleButton:
+        text: 'Play'
+        on_press: camera.play = not camera.play
         size_hint_y: None
-        height: "48dp"
-        spacing: "2dp"
-        padding: "2dp"
-
-        ToggleButton:
-            text: "Debug"
-            id: debug
-            on_release: root.build()
-        Button:
-            text: "New shape"
-            on_release: root.push_shape()
-        Button:
-            text: "Build"
-            on_release: root.build()
-        Button:
-            text: "Reset"
-            on_release: root.reset()
-
-    BoxLayout:
+        height: '48dp'
+    Button:
+        text: 'Capture'
         size_hint_y: None
-        height: "48dp"
-        top: root.top
-        spacing: "2dp"
-        padding: "2dp"
-        Label:
-            id: status
-            text: "Status"
-""")
+        height: '48dp'
+        on_press: root.capture()
+    Button:
+        id: button_change_image_state
+        text: 'Image state: 0'
+        size_hint_y: None
+        height: '48dp'
+        on_press: root.change_image_state()
+    MDTextField:
+        id: input_colors
+        hint_text: "Input colors"
+        text: "80, 100, 220; 120, 255, 255"
+        mode: "fill"
+        fill_color: 0, 0, 0, .4
+    Image:
+        id: image
+"""
 
 
-class ShapeBuilder(FloatLayout):
-    def __init__(self, **kwargs):
-        super(ShapeBuilder, self).__init__(**kwargs)
-        self.shapes = [
-            [100, 100, 300, 100, 300, 300, 100, 300],
-            [150, 150, 250, 150, 250, 250, 150, 250]
-        ]  # the 'hollow square' shape
-        self.shape = []
-        self.build()
+class CameraClick(BoxLayout):
+    image_state = 0
+    clock_is_ticking = False
 
-    def on_touch_down(self, touch):
-        if super(ShapeBuilder, self).on_touch_down(touch):
-            return True
-        self.shape.extend(touch.pos)
-        self.build()
-        return True
+    def clock_tick(self, dt):
+        self.capture()
 
-    def on_touch_move(self, touch):
-        if super(ShapeBuilder, self).on_touch_move(touch):
-            return True
-        self.shape.extend(touch.pos)
-        self.build()
-        return True
+    def capture(self):
+        if not self.clock_is_ticking:
+            self.clock_is_ticking = True
+            Clock.schedule_interval(self.clock_tick, 1.0 / 25)
+        
+        camera = self.ids['camera']
+        camtexture = camera.texture
 
-    def on_touch_up(self, touch):
-        if super(ShapeBuilder, self).on_touch_up(touch):
-            return True
-        self.push_shape()
-        self.build()
+        height, width = camtexture.height, camtexture.width
+        frame = np.frombuffer(camtexture.pixels, np.uint8)
+        frame = frame.reshape(height, width, 4)
 
-    def push_shape(self):
-        self.shapes.append(self.shape)
-        self.shape = []
 
-    def build(self):
-        tess = Tesselator()
-        count = 0
-        for shape in self.shapes:
-            if len(shape) >= 3:
-                tess.add_contour(shape)
-                count += 1
-        if self.shape and len(self.shape) >= 3:
-            tess.add_contour(self.shape)
-            count += 1
-        if not count:
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        org = (50, 50)
+        fontScale = 1
+        color = (0, 0, 0)
+        thickness = 2
+        cv2.putText(frame, 'OpenCV', org, font, 
+                    fontScale, color, thickness, cv2.LINE_AA)
+        if self.image_state % 7 == 0:
+            buf = cv2.flip(frame, -1)
+            buf = buf.tobytes()
+            texture = Texture.create(size=(frame.shape[1], frame.shape[0]))
+            texture.blit_buffer(buf, colorfmt='rgba')
+            self.ids['image'].texture = texture
             return
-        ret = tess.tesselate(WINDING_ODD, TYPE_POLYGONS)
-        self.canvas.after.clear()
 
-        debug = self.ids.debug.state == "down"
-        if debug:
-            with self.canvas.after:
-                c = 0
-                for vertices, indices in tess.meshes:
-                    Color(c, 1, 1, mode="hsv")
-                    c += 0.3
-                    indices = [0]
-                    for i in range(1, len(vertices) // 4):
-                        if i > 0:
-                            indices.append(i)
-                        indices.append(i)
-                        indices.append(0)
-                        indices.append(i)
-                    indices.pop(-1)
-                    Mesh(vertices=vertices, indices=indices, mode="lines")
-        else:
-            with self.canvas.after:
-                Color(1, 1, 1, 1)
-                for vertices, indices in tess.meshes:
-                    Mesh(vertices=vertices, indices=indices,
-                         mode="triangle_fan")
+        try:
+            colors = self.ids["input_colors"].text
+            colors = colors.split("; ")
+            colors[0] = colors[0].split(", ")
+            colors[1] = colors[1].split(", ")
+            colorLower = tuple(map(int, colors[0]))
+            colorUpper = tuple(map(int, colors[1]))
+        except:
+            colorLower = (None, )
+            colorUpper = (None, )
+        if len(colorLower) != 3 or len(colorUpper) != 3:
+            colorLower = (29, 86, 6)
+            colorUpper = (64, 255, 255)
 
-        self.ids.status.text = "Shapes: {} - Vertex: {} - Elements: {}".format(
-            count, tess.vertex_count, tess.element_count)
+        blurred = cv2.GaussianBlur(frame, (11, 11), 0)
+        hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+        mask = cv2.inRange(hsv, colorLower, colorUpper)
+        mask = cv2.erode(mask, None, iterations=2)
+        mask = cv2.dilate(mask, None, iterations=2)
+        cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+                                cv2.CHAIN_APPROX_SIMPLE)
+        cnts = imutils.grab_contours(cnts)
+        center = None
+        if len(cnts) > 0:
+            c = max(cnts, key=cv2.contourArea)
+            ((x, y), radius) = cv2.minEnclosingCircle(c)
+            M = cv2.moments(c)
+            center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+            if radius > 10:
+                cv2.circle(frame, (int(x), int(y)), int(radius),
+                           (0, 255, 255), 2)
+                cv2.circle(frame, center, 5, (0, 0, 255), -1)
 
-    def reset(self):
-        self.shapes = []
-        self.shape = []
-        self.ids.status.text = "Shapes: {} - Vertex: {} - Elements: {}".format(
-            0, 0, 0)
-        self.canvas.after.clear()
+            
+        buf = cv2.flip(frame, -1)
+        buf = buf.tobytes()
+        texture = Texture.create(size=(frame.shape[1], frame.shape[0]))
+        try:
+            if self.image_state % 7 == 1:
+                texture.blit_buffer(buf, colorfmt='rgba')
+            if self.image_state % 7 == 2:
+                texture.blit_buffer(buf, colorfmt='bgra')
+            if self.image_state % 7 == 3:
+                texture.blit_buffer(buf, colorfmt='rgb')
+            if self.image_state % 7 == 4:
+                texture.blit_buffer(buf, colorfmt='bgr')
+            if self.image_state % 7 == 5:
+                texture.blit_buffer(buf, colorfmt=camtexture.colorfmt)
+            if self.image_state % 7 == 6:
+                texture = camtexture
+            self.ids['image'].texture = texture
+        except:
+            pass
+
+    def change_image_state(self):
+        self.image_state += 1
+        self.ids["button_change_image_state"].text = 'Image state:' +\
+                                                    str(self.image_state % 7)
 
 
-class TestApp(MDApp):
+class TestCamera(MDApp):
     def build(self):
-        return ShapeBuilder()
+        if platform == "android":
+            from android.permissions import request_permissions, Permission
+            request_permissions([Permission.CAMERA,
+                                 Permission.WRITE_EXTERNAL_STORAGE,
+                                 Permission.READ_EXTERNAL_STORAGE])
+        Builder.load_string(mainkv)
+        return CameraClick()
 
 
-TestApp().run()
+TestCamera().run()
