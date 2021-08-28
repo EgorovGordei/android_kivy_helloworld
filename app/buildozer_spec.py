a8e6b0eae2160c59656984d71702cdbd45153c7d
EgorovGordei <78762468+EgorovGordei@users.noreply.github.com>
2021-08-28 16:39:51 +0300

    Update buildozer_spec.py

--- app/buildozer_spec.py
+++ app/buildozer_spec.py
@@ -5,7 +5,7 @@ f.close()
 s = s.replace("log_level = 1", "log_level = 2")
 s = s.replace("# android.accept_sdk_license = False", "android.accept_sdk_license = True")
 s = s.replace("#android.permissions = INTERNET", "android.permissions = INTERNET,CAMERA,WRITE_EXTERNAL_STORAGE,READ_EXTERNAL_STORAGE,RECORD_AUDIO,ACCESS_FINE_LOCATION,ACCESS_COARSE_LOCATION")
-s = s.replace("requirements = python3,kivy", "requirements = plyer,libffi,imutils,opencv,numpy,cffi,python3==3.7.9,hostpython3==3.7.9,kivy==master,kivymd==0.104.2,sdl2_ttf==2.0.15,pillow,requests,urllib3,charset-normalizer,idna,moviepy,youtube-dl,imageio,proglog")
+s = s.replace("requirements = python3,kivy", "requirements = plyer,libffi,imutils,opencv,numpy,cffi,python3==3.7.9,hostpython3==3.7.9,kivy==master,kivymd==0.104.2,sdl2_ttf==2.0.15,pillow,requests,urllib3,charset-normalizer,idna,moviepy,youtube-dl,imageio,proglog,tqdm")
 s = s.replace("#p4a.branch = master", "p4a.branch = develop")
 s = s.replace("osx.python_version = 3", "osx.python_version = 3.7.9")
 
