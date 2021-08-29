8b4c47f588bf8ad7324cca762bb2fb8aab37246d
EgorovGordei <78762468+EgorovGordei@users.noreply.github.com>
2021-08-29 11:42:04 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,15 +2,20 @@ import kivy
 from kivy.app import App 
 from kivy.uix.widget import Widget
 from kivy.graphics import Rectangle, Color
-from kivymd.app import MDApp
+from kivymd.app import FpsMonitoring, MDApp
+from kivy.clock import Clock
 
-from moviepy.editor import VideoFileClip
-link = "https://www.youtube.com/watch?v=4JkKGNFtmpQ"
-v = VideoFileClip(link)
+
+FPS = 60
 
 class CanvasWidget(Widget):
+    rect_movement_line = []
+
     def __init__(self, **kwargs):
+        global FPS
+        
         super(CanvasWidget, self).__init__(**kwargs)
+        Clock.schedule_interval(self.tick, 1.0/ FPS)
         with self.canvas:
             Color(1, 1, 1, 1) 
             self.rect = Rectangle(source ="Egypt_human_torches.png",
@@ -32,11 +37,26 @@ class CanvasWidget(Widget):
         self.rect.pos = self.pos
         self.rect.size = self.size
     
+    def tick(self, something):
+        if self.rect_movement_line != []:
+            self.rect2.pos = (self.rect_movement_line[0][0], self.rect_movement_line[0][1])
+            self.rect_movement_line = self.rect_movement_line[1:]
+
     def on_touch_down(self, touch):
-        self.rect2.pos = touch.pos
+        #self.rect2.pos = touch.pos
+        x = self.rect2.pos[0]
+        y = self.rect2.pos[1]
+        if self.rect_movement_line != []:
+            x = self.rect_movement_line[-1][0]
+            y = self.rect_movement_line[-1][1]
+        dx = (touch.x - x) / 100.0
+        dy = (touch.y - y) / 100.0
+        for i in range(100):
+            self.rect_movement_line.append([int(x + dx * i), int(y + dy * i)])
 
     def on_touch_move(self, touch):
-        self.rect2.pos = touch.pos
+        #self.rect2.pos = touch.pos
+        pass
 
 
 class CanvasApp(MDApp):
